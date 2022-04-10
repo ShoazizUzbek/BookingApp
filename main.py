88b8d62f805dc6bff6d580a3bc3db8a863cc4591
ShoazizUzbek <shoazizbakhtiyorov@gmail.com>
2022-04-10 13:14:39 +0500

    req update

--- main.py
+++ main.py
@@ -15,9 +15,11 @@ async def user_loader(token: str):
         return None
 
 
-application = web.Application(middlewares=[token_auth_middleware(
-    user_loader=user_loader,
-    exclude_routes=('/user/create', '/user/login')
-)])
+application = web.Application(
+    # middlewares=[token_auth_middleware(
+    # user_loader=user_loader,
+    # exclude_routes=('/user/create', '/user/login')
+# )]
+)
 setup_routes(application)
 web.run_app(application, port=port)
