d1987b7c08a934234859c31860fdb601bab47227
ShoazizUzbek <shoazizbakhtiyorov@gmail.com>
2022-04-10 13:58:23 +0500

    alembic

---
+++ alembic/versions/e4f71413db14_initial.py
@@ -0,0 +1,24 @@
+"""initial
+
+Revision ID: e4f71413db14
+Revises: 
+Create Date: 2022-04-10 13:40:05.454129
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e4f71413db14'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
